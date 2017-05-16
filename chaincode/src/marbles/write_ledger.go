0ebf1b130a441b6e501e142292f1724945af3341
sbachhety <sbachhety@prolifics.com>
2017-05-16 18:10:51 +0530

    docker file change

--- chaincode/src/marbles/write_ledger.go
+++ chaincode/src/marbles/write_ledger.go
@@ -178,8 +178,8 @@ func init_marble(stub shim.ChaincodeStubInterface, args []string) (pb.Response) 
 			"company": "` + owner.Company + `"
 		}
 	}`
-	err = stub.PutState(id, []byte(str))                         //store marble with id as key
-	if err != nil {
+	err =                         //store marble with id as key
+	if err != nil {stub.PutState(id, []byte(str)) 
 		return shim.Error(err.Error())
 	}
 
